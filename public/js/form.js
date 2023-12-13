5ab8c7d7d3adaeb446f560bde5c35bec8d4e846c
ayaojo <ojokuki01@gmail.com>
2023-12-14 05:48:05 +0600

    i did registration

--- public/js/form.js
+++ public/js/form.js
@@ -1,46 +1,47 @@
-const userName = document.querySelector('.name') || null;
+const user = document.querySelector('.name') || null;
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const submitBtn = document.querySelector('.submit-btn');
 
-if (userName == null) { // means login page is open
-     submitBtn.addEventListener('click', () => {
-        fetch('/login-user', {
+if(user == null){ // means login page is open
+    submitBtn.addEventListener('click', () => {
+        fetch('/login-user',{
             method: 'post',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
+            headers: new Headers({'Content-Type': 'application/json'}),
             body: JSON.stringify({
                 email: email.value,
                 password: password.value
             })
         })
-            .then(res => res.json())
-            .then(data => {
-                validateData(data);
-            })
-    }) 
-} else { // means register page is open
+        .then(res => res.json())
+        .then(data => {
+            validateData(data);
+        })
+    })
+} else{ // means register page is open
 
     submitBtn.addEventListener('click', () => {
         fetch('/register-user', {
             method: 'post',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
+            headers: new Headers({'Content-Type': 'application/json'}),
             body: JSON.stringify({
-                name: userName.value,
+                name: user.value,
                 email: email.value,
                 password: password.value
             })
         })
-            .then(res => res.json())
-            .then(data => {
-                validateData(data); 
-            })
+        .then(res => res.json())
+        .then(data => {
+            validateData(data);
+        })
     })
+
 }
 
- const validateData = (data) => {
-    if (!data.name) {
+const validateData = (data) => {
+    if(!data.name){
         alertBox(data);
-    } else {
+    } else{
         sessionStorage.name = data.name;
         sessionStorage.email = data.email;
         location.href = '/';
@@ -56,4 +57,4 @@ const alertBox = (data) => {
     setTimeout(() => {
         alertContainer.style.top = null;
     }, 5000);
-}  
+}
---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'localhost', // Вместо 'local', используйте 'localhost'
+        user: 'postgres',
+        password: 'zhan',
+        database: 'registerofrm'
+    }
+});
+
+const app = express();
+
+let initialPath = path.join(__dirname, "public");
+
+app.use(bodyParser.json());
+app.use(express.static(initialPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initialPath, "profile.html"));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(initialPath, "login.html"));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(initialPath, "register.html"));
+});
+
+app.post('/register-user', (req, res) => {
+    const { name, email, password } = req.body; // Используйте 'name' вместо 'user'
+
+    if (!name || !email || !password) {
+        res.json('Fill in all the fields');
+    } else {
+        db("users").insert({
+            name: name,
+            email: email,
+            password: password
+        })
+        .returning(["name", "email"])
+        .then(data => {
+            res.json(data[0]);
+        })
+        .catch(err => {
+            if (err.detail && err.detail.includes('already exists')) {
+                res.json('Email already exists');
+            } else {
+                res.status(500).json('Internal Server Error');
+            }
+        });
+    }
+});
+
+app.post('/login-user', (req, res) => {
+    const { email, password } = req.body;
+
+    db.select('name', 'email')
+        .from('users')
+        .where({
+            email: email,
+            password: password
+        })
+        .then(data => {
+            if (data.length) {
+                res.json(data[0]);
+            } else {
+                res.json('Email or password is incorrect');
+            }
+        })
+        .catch(err => {
+            res.status(500).json('Internal Server Error');
+        });
+});
+
+app.listen(5501, () => {
+    console.log('Listening on port 5501......');
+});
